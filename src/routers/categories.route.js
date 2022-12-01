1deed4a72dce1396d9912651e6082070892e4948
Ecorralesz <estebcz@hotmail.com>
2022-12-01 09:54:13 -0600

    updated the routes

--- src/routers/categories.route.js
+++ src/routers/categories.route.js
@@ -3,25 +3,6 @@ const categoriesController = require("../controllers/categoriesController")
 
 const router = Router()
 
-/**
- * 
- * @swagger
- * components:
- *   schemas:
- *     Category:
- *       type: object
- *       properties:
- *         name:
- *           type: string
- *           description: specific name for the category
- *         
- *       required:
- *         - name
- *       example:
- *         name: tech
- *   
- * 
- */
 
 router.get("/categories", categoriesController.findAll)
 router.get("/categories/:id", categoriesController.findById)
--- src/routers/products.route.js
+++ src/routers/products.route.js
@@ -3,50 +3,6 @@ const productsController = require("../controllers/products.controller")
 
 const router = Router()
 
-/**
- * 
- * @swagger
- * components:
- *   schemas:
- *     Product:
- *       type: object
- *       properties:
- *         name:
- *           type: string
- *           description: the product name
- *         description:
- *           type: string
- *           description: the product description
- *         stock:
- *           type: number
- *           description: the product stock
- *       required:
- *         - name
- *         - stock
- *       example:
- *         name: Redmi 8
- *         description: Xiaomi Redmi 8 smarthphone
- *         stock: 0
- * 
- */
-
-/**
- * @swagger
- * /api/v1/products:
- *   get:
- *     summary: list of all products
- *     tags: [Product]
- *     responses:
- *       200:
- *         description: all list was fetched
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items: 
- *                 $ref: '#/components/schemas/Product'
- *                 
- */
 router.get("/products", productsController.findAll)
 
 router.get("/products/:id", productsController.findById)
