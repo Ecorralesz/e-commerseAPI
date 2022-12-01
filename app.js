4775bbff6879f58451db6c87f45ae7475005da8a
Ecorralesz <estebcz@hotmail.com>
2022-12-01 10:07:20 -0600

    updated app

--- app.js
+++ app.js
@@ -17,22 +17,6 @@ const authenticate = require("./src/middlewares/auth.middleware")
 
 const initData = require("./src/seeders/initData")
 
-//swagger
-const swaggerUI = require("swagger-ui-express")
-const swaggerJsDoc = require("swagger-jsdoc")
-const swaggerSpecs = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "Ecommerce",
-            version: "1.0.0"
-        },
-        servers: [
-            { url: "http://localhost:8000" }
-        ]
-    },
-    apis: [`${path.join(__dirname, "./src/routers/*.js")}`]
-}
 
 const app = express()
 
@@ -69,11 +53,6 @@ app.use("/api/v1", authenticate, productRouter)
 app.use("/api/v1", authenticate, featureRouter)
 app.use("/api/v1", authenticate, cartRouter)
 
-app.use(
-    "/api-doc",
-    swaggerUI.serve,
-    swaggerUI.setup(swaggerJsDoc(swaggerSpecs))
-)
 
 app.use(errorHandler)
 
